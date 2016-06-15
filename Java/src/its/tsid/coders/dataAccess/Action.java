77169000e5d9034f5c3404f5780229a38420bd8b
gryphon87 <Filippo.Bozzola@gmail.com>
2016-06-15 11:42:54 +0200

    Cleanup of the DbAccess class

---
+++ Java/src/its/tsid/coders/dataAccess/Action.java
@@ -0,0 +1,5 @@
+package its.tsid.coders.dataAccess;
+
+public class Action {
+	
+}
--- Java/src/its/tsid/coders/dataAccess/DbAccess.java
+++ Java/src/its/tsid/coders/dataAccess/DbAccess.java
@@ -1,8 +1,42 @@
 package its.tsid.coders.dataAccess;
 
+import java.sql.*;
+import its.tsid.coders.dataAccess.Action;
 
 public class DbAccess {
+	private static Connection conn = null;
+	private static Statement stmt = null;
+	private static String db = "jdbc:postgresql://localhost:5432/test";
+	private static String user = "postgres";
+	private static String pwd = "root";
 
+	public DbAccess(String db, String user, String pwd) {
+		this.db = db;
+		this.user = user;
+		this.pwd = pwd;
+	}
 
+	public static void dbAccess(String table, Action action) {
+
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			conn = DriverManager.getConnection(db, user, pwd);
+			conn.setAutoCommit(false);
+			String sql = "SELECT id FROM retrieved;";
+
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sql);
+
+			conn.commit();
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
