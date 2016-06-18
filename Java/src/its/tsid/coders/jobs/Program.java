eb1e18c9dc15a2b87ee6647ffd1a7c9f6d3bbad5
gryphon87 <Filippo.Bozzola@gmail.com>
2016-06-18 18:50:05 +0200

    Analysis class setup

---
+++ Java/src/its/tsid/coders/jobs/Program.java
@@ -0,0 +1,10 @@
+package its.tsid.coders.jobs;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/src/its/tsid/coders/jobs/tests/AllTests.java
@@ -0,0 +1,14 @@
+package its.tsid.coders.jobs.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AllTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
