991db1c89476b2d80f4043c34b9abf440f03d5e0
gryphon87 <Filippo.Bozzola@gmail.com>
2016-06-18 20:33:51 +0200

    program check for usr & pwd for the db

--- Java/src/its/tsid/coders/jobs/Program.java
+++ Java/src/its/tsid/coders/jobs/Program.java
@@ -3,8 +3,15 @@ package its.tsid.coders.jobs;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String message = "";
+		if (args.length == 0) {
+			message = "Error, need to pass username and password for the db!";
+		} else {
+			if (args.length != 2) {
+				message = String.format("Error, expected 2 parameters! " + "Got %i parameters!", args.length);
+			}
+		}
+		System.out.println(message);
 	}
 
 }
