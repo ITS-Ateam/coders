78cb06a7b07d57fdc7d853965fe03f3b320fa88e
gryphon87 <Filippo.Bozzola@gmail.com>
2016-07-14 22:21:29 +0200

    Edited category listing

--- Java/src/its/tsid/coders/analysis/Category.java
+++ Java/src/its/tsid/coders/analysis/Category.java
@@ -2,7 +2,7 @@ package its.tsid.coders.analysis;
 
 public class Category {
 	public static enum category  {
-		LEISURE, SALARY, PAY, GREEN, CULTURE, SAFETY, INNOVATION
+		LEISURE, SALARY, JOBS, GREEN, CULTURE, SAFETY, INNOVATION
 }
 
 }
